c247dd6fbd70e17fb5f0148cfe01df9647d17f37
Dumitru Ursu <dima@ceata.org>
2013-10-16 10:17:29 +0300

    Use locales

---
+++ 4/gui.glade.h
@@ -0,0 +1,6 @@
+char *s = N_("_File");
+char *s = N_("_Edit");
+char *s = N_("_View");
+char *s = N_("_Help");
+char *s = N_("Cut");
+char *s = N_("Paste");
--- 4/script.py
+++ 4/script.py
@@ -1,10 +1,17 @@
 #! /usr/bin/env python
 from gi.repository import Gtk
+import locale, gettext
+
+LOCALE_DIR="locale"
 
 class GUI(Gtk.Window):
     def __init__(self):
-        title="A gui program"
+        title="A localized gui program"
         builder = Gtk.Builder()
+        # localisation stuff
+        language = gettext.translation ('default', LOCALE_DIR, ["ro"])
+        language.install()
+        builder.set_translation_domain('default')
         # import the glade file
         builder.add_from_file("gui.glade")
         # connect the handlers
@@ -17,7 +24,7 @@ class Handler:
         Gtk.main_quit(*args)
 
     def on_Cut_clicked(self, button):
-        print("Hello World!")
+        print(_("Hello World!"))
 
 GUI()
 Gtk.main()
