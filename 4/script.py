a7b2f72dcf72349e7ccd38fc050d7786d2bac019
Dumitru Ursu <dima@ceata.org>
2013-10-22 10:05:01 +0300

    translation still not applying

--- 4/script.py
+++ 4/script.py
@@ -1,30 +1,34 @@
 #! /usr/bin/env python
 from gi.repository import Gtk
-import locale, gettext
+import locale
 
 LOCALE_DIR="locale"
+TRANSLATION_DOMAIN = "default"
 
 class GUI(Gtk.Window):
     def __init__(self):
         title="A localized gui program"
-        builder = Gtk.Builder()
         # localisation stuff
-        language = gettext.translation ('default', LOCALE_DIR, ["ro"])
-        language.install()
-        builder.set_translation_domain('default')
+        locale.bindtextdomain(TRANSLATION_DOMAIN, LOCALE_DIR)
+        locale.setlocale(locale.LC_ALL, "")
         # import the glade file
+        builder = Gtk.Builder()
+        builder.set_translation_domain(TRANSLATION_DOMAIN)
         builder.add_from_file("gui.glade")
         # connect the handlers
         builder.connect_signals(Handler())
         window = builder.get_object("MainWindow")
         window.show_all()
 
+
+
+
 class Handler:
     def on_quit(self, *args):
         Gtk.main_quit(*args)
 
     def on_Cut_clicked(self, button):
-        print(_("Hello World!"))
+        print("Hello World!")
 
 GUI()
 Gtk.main()
